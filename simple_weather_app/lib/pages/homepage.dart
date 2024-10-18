14e64704d3c4a5a7ab23dfd4a86968dbcc28db70
Juno-whut <owenrupnow17@gmail.com>
2024-10-18 15:13:36 -0500

    finished tutorial but am going to try adding in city search at the top for changing locations and maybe add in dynamic background depending on weather conditions ie rain for ran sunny for sunny

--- simple_weather_app/lib/pages/homepage.dart
+++ simple_weather_app/lib/pages/homepage.dart
@@ -12,9 +12,9 @@ class Homepage extends StatefulWidget{
 
 class _HomepageState extends State<Homepage>{
   final WeatherFactory _wf = WeatherFactory(OPENWEATHER_API_KEY);
-
   Weather? _weather;
 
+  
   @override
   void initState() {
     super.initState();
@@ -48,17 +48,29 @@ class _HomepageState extends State<Homepage>{
             height: MediaQuery.sizeOf(context).height * 0.08,
           ),
           _dateTimeInfo(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.02,
+          ),
+          _weatherIcon(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.02,
+          ),
+          _currentTemp(),
+          SizedBox(height: MediaQuery.sizeOf(context).height  * 0.02,
+          ),
+          _extraInfo(),
         ],
       )
     );
   }
 
   Widget _locationHeader() {
-    return Text(_weather?.areaName ?? "",
-    style: const TextStyle(
-      fontSize: 20,
-      fontWeight: FontWeight.w500,
-    ),
+    return Text(
+      _weather?.areaName ?? "",
+      style: const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+      ),
     );
   }
 
@@ -95,6 +107,97 @@ class _HomepageState extends State<Homepage>{
     ]
     )
     ],
+    );
+  }
+
+  Widget _weatherIcon() {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          height: MediaQuery.sizeOf(context).height * 0.2,
+          decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage("https://openweathermap.org/img/wn/${_weather?.weatherIcon}@4x.png",
+            ),
+          ),
+        ),
+        ),
+        Text(_weather?.weatherDescription ?? "",
+        style: const TextStyle(
+          color: Colors.black,
+          fontSize: 20, 
+        ),
+        ),
+      ],
+    );
+  }
+
+  Widget _currentTemp() {
+    return Text("${_weather?.temperature?.fahrenheit?.toStringAsFixed(0)}°F",
+        style: const TextStyle(
+          color: Colors.black,
+          fontSize: 90,
+          fontWeight: FontWeight.w500,
+        )
+      );       
+  }
+
+  Widget _extraInfo() {
+    return Container(
+      width: MediaQuery.sizeOf(context).width * 0.8,
+      height: MediaQuery.sizeOf(context).height * 0.15,
+      decoration: BoxDecoration
+      (color: Colors.lightBlueAccent, 
+      borderRadius: BorderRadius.circular(15),
+      ),
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("High: ${_weather?.tempMax?.fahrenheit?.toStringAsFixed(0)}°F",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                ),
+              ),
+              Text("Low: ${_weather?.tempMin?.fahrenheit?.toStringAsFixed(0)}°F",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize:15,
+              ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Wind: ${(_weather?.windSpeed != null ? _weather!.windSpeed! * 2.23694 : 0).toStringAsFixed(0)} mph",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                ),
+              ),
+              Text("Humidity: ${_weather?.humidity?.toStringAsFixed(0)}%",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize:15,
+              ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
