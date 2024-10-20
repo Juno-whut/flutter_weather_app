be685dbe71e912b4d8505520b81316f815b73482
Juno-whut <owenrupnow17@gmail.com>
2024-10-20 17:45:49 -0400

    added a weather_service.dart for getting the weather data

---
+++ simple_weather_app/lib/service/weather_service.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import '../models/weather_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+
+class WeatherService {
+  static const BASE_URL = 'https://openweathermap.org/';
+  final String apiKey;
+
+  WeatherService(this.apiKey);
+
+  Future<Weather> getWeather(String cityName) async {
+    final response = await http.get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric')); // try changing to imperial later
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load weather');
+    }
+  }
+
+  Future<String> getCurrentCity() async {
+
+    // get permission from user
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    // fetch the current location
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high);
+
+    // convert location in a list of placement mark objects
+    List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    // extract city name from the first placement mark
+    String? city = placemarks[0].locality;
+
+    return city ?? '';
+  }
+}
